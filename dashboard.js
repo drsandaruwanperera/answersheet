47cbb1c6656b07c1e87949120909d1820060a554
nimethdeemantha8-boop <nimethdeemantha8@gmail.com>
2026-07-29 13:39:36 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,33 @@
-function openPaper(no){
+import { db, doc, getDoc, updateDoc } from "./firebase.js";
 
-window.location.href="viewer.html?paper="+no;
+async function openPaper(no) {
 
+    const params = new URLSearchParams(window.location.search);
+    const studentId = params.get("id");
+
+    const ref = doc(db, "students", studentId);
+    const snap = await getDoc(ref);
+
+    if (!snap.exists()) {
+        alert("Student not found");
+        return;
+    }
+
+    const data = snap.data();
+
+    const field = "paper" + String(no).padStart(2, "0");
+
+    if (data[field] === true) {
+        alert("This Model Paper has already been viewed.");
+        return;
+    }
+
+    await updateDoc(ref, {
+        [field]: true
+    });
+
+    window.location.href =
+        "viewer.html?paper=" + no;
 }
+
+window.openPaper = openPaper;
