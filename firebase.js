074de845f7c99d3f3f394b1910cc4f15b40a08aa
nimethdeemantha8-boop <nimethdeemantha8@gmail.com>
2026-07-29 13:39:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,23 +8,15 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyAKbrx_tt4SXi1obmDIvqUVi2VmcXCs0_8",
-
+  apiKey: "...",
   authDomain: "answersheet2026.firebaseapp.com",
-
   projectId: "answersheet2026",
-
   storageBucket: "answersheet2026.firebasestorage.app",
-
   messagingSenderId: "953495846284",
-
   appId: "1:953495846284:web:0f1f9def812a5cbef16aa9"
-
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export { db, doc, getDoc, updateDoc };
