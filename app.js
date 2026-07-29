bf53c76bc447eb0d6c5c01d43f3c3ef7ac672a6d
nimethdeemantha8-boop <nimethdeemantha8@gmail.com>
2026-07-29 12:19:49 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+import { db, doc, getDoc, updateDoc } from "./firebase.js";
 
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+    const studentId = document.getElementById("studentId").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const msg = document.getElementById("msg");
+
+    if (studentId === "" || password === "") {
+        msg.innerHTML = "Please enter Student ID and Password";
+        return;
+    }
+
+    const ref = doc(db, "students", studentId);
+
+    const snap = await getDoc(ref);
+
+    if (!snap.exists()) {
+        msg.innerHTML = "Student ID not found";
+        return;
+    }
+
+    const data = snap.data();
+
+    if (data.password !== password) {
+        msg.innerHTML = "Wrong Password";
+        return;
+    }
+
+    if (data.used === true) {
+        msg.innerHTML = "Password Already Used";
+        return;
+    }
+
+    await updateDoc(ref, {
+        used: true
+    });
+
+    window.location.href = "answer.html";
+
+});
