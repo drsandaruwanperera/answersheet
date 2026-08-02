ae4d3995cd92e1c4ea6350f4cda610ba8729f4a0
Dr. Sandaruwan Perera <nimethdeemantha8@gmail.com>
2026-08-02 11:55:50 +0530

    Update answer-images.js

--- answer-images.js
+++ answer-images.js
@@ -1,1 +1,23 @@
+const params = new URLSearchParams(window.location.search);
 
+const paper = params.get("paper");
+const type = params.get("type");
+
+const container = document.getElementById("imageContainer");
+
+// Maximum pages to check
+for (let i = 1; i <= 30; i++) {
+
+    const page = String(i).padStart(2, "0");
+
+    const img = document.createElement("img");
+
+    img.src = `answers/paper${paper}/${type}/page${page}.jpg`;
+
+    img.className = "answer-image";
+
+    img.onload = () => {
+        container.appendChild(img);
+    };
+
+}
