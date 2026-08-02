35a360b89778b272c6349de0a1737cb8fa50f5bd
Dr. Sandaruwan Perera <nimethdeemantha8@gmail.com>
2026-08-02 14:05:23 +0530

    Update province-paper2.js

--- province-paper2.js
+++ province-paper2.js
@@ -1,1 +1,43 @@
+const container = document.getElementById("paperContainer");
 
+const provinces = [
+    {
+        name: "Central Province",
+        link: "province-paper1-detail.html?province=central"
+    },
+    {
+        name: "Western Province",
+        link: "province-paper1-detail.html?province=western"
+    },
+    {
+        name: "North Western Province",
+        link: "province-paper1-detail.html?province=north-western"
+    },
+    {
+        name: "Southern Province",
+        link: "province-paper1-detail.html?province=southern"
+    },
+    {
+        name: "Sabaragamuwa Province",
+        link: "province-paper1-detail.html?province=sabaragamuwa"
+    }
+];
+
+provinces.forEach(item => {
+
+    const card = document.createElement("div");
+    card.className = "paper-card";
+    card.style.cursor = "pointer";
+
+    card.onclick = () => {
+        location.href = item.link;
+    };
+
+    card.innerHTML = `
+        <h2>📁 ${item.name}</h2>
+        <p>MCQ Paper & MCQ Answer Scheme</p>
+    `;
+
+    container.appendChild(card);
+
+});
