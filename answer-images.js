f8a4249e39d312d20d6e2346e1c2275148a43148
Dr. Sandaruwan Perera <nimethdeemantha8@gmail.com>
2026-08-02 12:06:02 +0530

    Update answer-images.js

--- answer-images.js
+++ answer-images.js
@@ -5,42 +5,25 @@ const type = params.get("type");
 
 const container = document.getElementById("imageContainer");
 
-// Maximum pages to check
 for (let i = 1; i <= 30; i++) {
 
     const page = String(i).padStart(2, "0");
 
+    const card = document.createElement("div");
+    card.className = "answer-page";
+
+    const title = document.createElement("div");
+    title.className = "page-title";
+    title.textContent = "Page " + page;
+
     const img = document.createElement("img");
-
     img.src = `answers/paper${paper}/${type}/page${page}.jpg`;
-
     img.className = "answer-image";
 
     img.onload = () => {
-        container.appendChild(img);
-    };
-
-}
-const params = new URLSearchParams(window.location.search);
-
-const paper = params.get("paper");
-const type = params.get("type");
-
-const container = document.getElementById("imageContainer");
-
-// Maximum pages to check
-for (let i = 1; i <= 30; i++) {
-
-    const page = String(i).padStart(2, "0");
-
-    const img = document.createElement("img");
-
-    img.src = `answers/paper${paper}/${type}/page${page}.jpg`;
-
-    img.className = "answer-image";
-
-    img.onload = () => {
-        container.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(img);
+        container.appendChild(card);
     };
 
 }
