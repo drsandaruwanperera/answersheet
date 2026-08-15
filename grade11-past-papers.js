37cfa6b91dcb392bf087b84d914095466fd2b8d3
Dr. Sandaruwan Perera <nimethdeemantha8@gmail.com>
2026-08-15 07:51:43 +0530

    Update grade11-past-papers.js

--- grade11-past-papers.js
+++ grade11-past-papers.js
@@ -1,51 +1,81 @@
+// ==========================
+// Elements
+// ==========================
+
 const yearContainer =
     document.getElementById(
         "yearContainer"
     );
 
 
-for (
-    let year = 2025;
-    year >= 2016;
-    year--
-) {
+// ==========================
+// Create Years
+// ==========================
 
-    const card =
-        document.createElement("div");
-
-    card.className =
-        "year-card";
-
-
-    card.innerHTML = `
-
-        <div class="year-icon">
-            📖
-        </div>
-
-        <h2>
-            ${year}
-        </h2>
-
-        <p>
-            Grade 11 Past Paper
-        </p>
-
-    `;
+const years = [
+    2025,
+    2024,
+    2023,
+    2022,
+    2021,
+    2020,
+    2019,
+    2018,
+    2017,
+    2016
+];
 
 
-    card.addEventListener(
-        "click",
-        () => {
+// ==========================
+// Create Year Cards
+// ==========================
 
-            window.location.href =
-                "grade11-past-paper.html?year=" +
-                year;
+years.forEach(
+    year => {
 
-        }
-    );
+        const card =
+            document.createElement(
+                "div"
+            );
+
+        card.className =
+            "year-card";
 
 
-    yearContainer.appendChild(card);
+        card.innerHTML = `
 
-}
+            <div class="year-icon">
+                📖
+            </div>
+
+            <h2>
+                ${year}
+            </h2>
+
+            <p>
+                Grade 11 Past Paper
+            </p>
+
+        `;
+
+
+        card.addEventListener(
+            "click",
+            () => {
+
+                window.location.href =
+                    "grade11-past-paper.html?year=" +
+                    encodeURIComponent(
+                        year
+                    );
+
+            }
+        );
+
+
+        yearContainer.appendChild(
+            card
+        );
+
+    }
+);
